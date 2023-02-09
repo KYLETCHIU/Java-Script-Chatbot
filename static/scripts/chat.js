baa62cdcb31ede4ad00156bca2ed1e963ea9f3e5
Erin McLear <erin.a.mclear@gmail.com>
2023-02-09 12:16:52 -0500

    eep

--- static/scripts/chat.js
+++ static/scripts/chat.js
@@ -1,18 +1,16 @@
-// Makes chat collapsible
 
-
-// // Grabs and displays first message
-// // function firstBotMessage() {
+// Grabs and displays first message
+// function firstBotMessage() {
 //     let firstMessage = "How may I assist you?"
-//     // document.getElementById("botStarterMessage").innerHTML = '<p class="botText"><span>' + firstMessage + '</span></p>';
+//     document.getElementById("botStarterMessage").innerHTML = '<p class="botText"><span>' + firstMessage + '</span></p>';
 
 //     let time = getTime();
 
 //     $("#chat-timestamp").append(time);
 //     document.getElementById("userInput").scrollIntoView(false);
-// // }
+// }
 
-//firstBotMessage();
+// firstBotMessage();
 
 // Makes chat collapsible
 var coll = document.getElementsByClassName("collapsible");
@@ -39,6 +37,29 @@ let sendBtn = document.getElementById("sendBtn");
 let inputGroup = document.getElementById("inputGroup");
 
 
+sendBtn.addEventListener("click", () => {
+    console.log('click');
+    let input = userInput.value;
+
+    if (userInput.value !== '') {
+        postUserInput();
+        getAnswers(input);
+    }  
+})
+
+
+inputGroup.addEventListener("keyup", (e) => {
+    
+    if(e.keyCode === 13){
+
+        let input = userInput.value;
+
+        if (userInput.value !== '') {
+            postUserInput();
+            getAnswers(input);
+        }  
+    }
+})
 
 
 let postUserInput = () => {
@@ -67,24 +88,44 @@ let getAnswers = (input) => {
 
 let searchAnswersInDB = (input, questions) => {
 
-    // buttonsDiv.innerHTML = ""; 
+    
+    
+    for (question of questions) {
 
-    for (let question of questions) {
-        if (question.keywords.some( a => input.toLowerCase().includes(a.toLowerCase()))) {
+        let i = 0;
 
-            chatHistory.innerHTML +=`<div class="botText">${question.answer}</div>`;
+        var allKeys = question.keywords[i];  
+        var allKeysLength = question.keywords.length;
+        var questionsLength = questions.length;
 
+        console.log(input);
+        console.log(allKeysLength);
+        console.log(questionsLength);
+        console.log(allKeys);
+        
+        while (i < questionsLength) {
+        
+
+            if (question.keywords.some(a => input.toLowerCase().includes(a.toLowerCase()))) {
+
+                chatHistory.innerHTML +=`<div class="botText">${question.answer}</div>`;
+            
+            
            
-            chatHistory.scrollIntoView({block: 'end', behavior: 'smooth'});
-            buttonsDiv.scrollIntoView({block: 'end', behavior: 'smooth'});
-                console.log(question);
-            return question;
+                chatHistory.scrollIntoView({block: 'end', behavior: 'smooth'});
+                buttonsDiv.scrollIntoView({block: 'end', behavior: 'smooth'});
+                    console.log(question);
+                return question;
+    
+            } else if (question.keywords.some( a => input.toLowerCase().includes(a.toLowerCase())) === {}) {
+    
+                chatHistory.innerHTML +=`<div class="chatBubblesBot">I don't understand this, please be more precise.</div>`;
+            } 
 
-        } else if (question.keywords.some( a => input.toLowerCase().includes(a.toLowerCase())) === {}) {
+            i++;
 
-            chatHistory.innerHTML +=`<div class="chatBubblesBot">I don't understand this, please be more precise.</div>`;
-        } 
+            }
+        }     
         
     }  
  
-}
